ef1b1aa55c83fa1b99a00d31182e223cc241afbe
SIXWERS <nazzygghggh@gmail.com>
2026-09-04 00:47:24 +0500

    day 1

---
+++ Program.cs
@@ -0,0 +1,9 @@
+string name;
+name = "alex";
+System.Console.WriteLine("имя:"+name);
+int level;
+level = 5;
+System.Console.WriteLine("уровень:"+level);
+int health;
+health = 97;
+System.Console.WriteLine("здоровье:"+health);
