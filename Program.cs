941fd1ab2c45fe16f4ffc9fac662c0fdaab58e04
SIXWERS <nazzygghggh@gmail.com>
2026-09-08 22:30:12 +0500

    day 2

--- Program.cs
+++ Program.cs
@@ -2,12 +2,34 @@
 int hp;
 int dmg;
 
-
 System.Console.WriteLine("Введите базовое здоровье (HP):");
  hp = int.Parse(System.Console.ReadLine());
  int maxhp = hp + 50;
+if(hp >= 80) 
+System.Console.WriteLine("персонаж целый");
+
+ else if(hp >=21)
+ System.Console.WriteLine("персонаж ранен");
+
+ else 
+System.Console.WriteLine("персонаж при смерти");
+
+ 
+
+ 
+
+
+
+
+
+
+
+
+
+
  System.Console.WriteLine("Введите базовый урон:");
 dmg = int.Parse(System.Console.ReadLine());
  int critdmg = dmg * 2;
  System.Console.WriteLine($"максимальное здоровье: {maxhp}");
  System.Console.WriteLine($"критический урон: {critdmg}");
+
