7fc3f99fd9da55c7e5a529339b593f8e115acecd
SIXWERS <nazzygghggh@gmail.com>
2026-09-08 16:52:40 +0500

    day 2

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
-string name;
-name = "alex";
-System.Console.WriteLine("имя:"+name);
-int level;
-level = 5;
-System.Console.WriteLine("уровень:"+level);
-int health;
-health = 97;
-System.Console.WriteLine("здоровье:"+health);
+string namestring;
+string characterClass;
+string weapon;
+System.Console.WriteLine("введите имя персонажа:");
+namestring = System.Console.ReadLine();
+
+System.Console.WriteLine("Введите класс (Воин, Маг, Лучник):");
+characterClass = System.Console.ReadLine();
+
+System.Console.WriteLine("Введите оружие:");
+weapon = System.Console.ReadLine();
+
+System.Console.WriteLine($"Имя: {namestring}");
+System.Console.WriteLine($"класс: {characterClass}");
+System.Console.WriteLine($"оружие: {weapon}");
